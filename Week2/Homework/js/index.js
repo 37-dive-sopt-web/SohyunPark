344de6ded605b188885e230561217233deae7ce7
Sohyunnnn <thgus05061@naver.com>
2025-10-31 01:53:34 +0900

    feat: 필터링 기능 구현

--- Week2/Homework/js/index.js
+++ Week2/Homework/js/index.js
@@ -1,6 +1,9 @@
 import { loadMembers, saveMembers, clearMembers } from "./storage.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".search");
+  const deleteBtn = document.querySelector("#delete-btn");
+  const addBtn = document.querySelector("#add-btn");
   const tbody = document.querySelector(".table-section__table-body");
   let members = loadMembers();
 
@@ -23,4 +26,44 @@ document.addEventListener("DOMContentLoaded", () => {
       tbody.appendChild(tr);
     });
   }
+
+  renderTable(members);
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = document.querySelector("#name").value.trim();
+    const englishName = document.querySelector("#englishName").value.trim();
+    const github = document.querySelector("#githubId").value.trim();
+    const gender = document.querySelector("#gender").value;
+    const role = document.querySelector("#role").value;
+    const codeReviewGroup = parseInt(
+      document.querySelector("#codeReviewGroup").value,
+      10
+    );
+    const age = parseInt(document.querySelector("#age").value, 10);
+
+    const filteredMembers = members.filter((member) => {
+      return (
+        (name === "" || member.name.includes(name)) &&
+        (!englishName ||
+          member.englishName
+            .toLowerCase()
+            .includes(englishName.toLowerCase())) &&
+        (!github ||
+          member.github.toLowerCase().includes(github.toLowerCase())) &&
+        (gender === "all" || member.gender === gender) &&
+        (role === "all" || member.role === role) &&
+        (isNaN(codeReviewGroup) ||
+          member.codeReviewGroup === codeReviewGroup) &&
+        (isNaN(age) || member.age === age)
+      );
+    });
+
+    renderTable(filteredMembers);
+  });
+
+  form.addEventListener("reset", () => {
+    renderTable(members);
+  });
 });
