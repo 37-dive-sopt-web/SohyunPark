3615701dd1626be0822d290c9c1d0d2fd55c6c88
Sohyunnnn <thgus05061@naver.com>
2025-11-11 23:16:19 +0900

    refactor: 버튼 스타일 수정

--- Week3/homework/src/components/Button.jsx
+++ Week3/homework/src/components/Button.jsx
@@ -1,6 +1,14 @@
-const Button = ({ children }) => {
+const Button = ({ children, active, onClick }) => {
   return (
-    <button className="bg-blue-300 rounded-xl px-3 py-1 cursor-pointer">
+    <button
+      onClick={onClick}
+      className={`rounded-xl px-3 py-1 font-medium transition 
+        ${
+          active
+            ? "bg-blue-400 text-white shadow-sm"
+            : "bg-blue-200 hover:bg-blue-300 text-gray-700"
+        }`}
+    >
       {children}
     </button>
   );
