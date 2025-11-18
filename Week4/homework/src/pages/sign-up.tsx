677e95bc18feabb83c7c915d6b4bbfc9af4ce125
Sohyunnnn <thgus05061@naver.com>
2025-11-19 03:23:29 +0900

    feat: 회원가입 페이지 구현

--- Week4/homework/src/pages/sign-up.tsx
+++ Week4/homework/src/pages/sign-up.tsx
@@ -1,5 +1,146 @@
+import { useState } from "react";
+import AuthForm from "../components/auth-form";
+import Label from "../components/label";
+import { useNavigate } from "react-router";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SignUpSchema } from "../schemas/sign-up-schema";
+
+type SignUpForm = z.infer<typeof SignUpSchema>;
+
 const SignUp = () => {
-  return <div>sign-up</div>;
+  const navigate = useNavigate();
+  const [step, setStep] = useState(1);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<SignUpForm>({
+    resolver: zodResolver(SignUpSchema),
+    mode: "onChange",
+  });
+
+  const id = watch("id");
+  const password = watch("password");
+  const passwordCheck = watch("passwordCheck");
+  const name = watch("name");
+  const email = watch("email");
+  const age = watch("age");
+
+  const handleClick = async () => {
+    setStep((prev) => Math.min(prev + 1, 3));
+  };
+
+  const handlePrev = () => {
+    setStep((prev) => {
+      if (prev <= 1) {
+        navigate("/sign-in");
+        return 1;
+      }
+      return prev - 1;
+    });
+  };
+
+  const onSubmit = (data: SignUpForm) => {
+    console.log("최종 회원가입 데이터:", data);
+    alert(`${data.name}님, 회원가입이 완료되었습니다!`);
+    navigate("/sign-in");
+  };
+
+  const isStepValid = (() => {
+    if (step === 1) return !!id;
+    if (step === 2) return !!password && !!passwordCheck;
+    if (step === 3) return !!name && !!email && !!age;
+  })();
+
+  return (
+    <div className="min-h-screen flex items-center w-full">
+      <AuthForm
+        title="회원가입"
+        linkTo="/sign-in"
+        linkText="로그인으로 돌아가기"
+        buttonText={step === 3 ? "회원가입" : "다음"}
+        buttonType={step === 3 ? "submit" : "button"}
+        onClick={step === 3 ? undefined : handleClick}
+        onPrev={handlePrev}
+        disabled={!isStepValid}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        {step === 1 && (
+          <>
+            <Label
+              label="아이디"
+              placeholder="아이디를 입력해 주세요."
+              {...register("id")}
+            />
+            {errors.id && (
+              <p className="text-red-500 text-sm">{errors.id.message}</p>
+            )}
+          </>
+        )}
+
+        {step === 2 && (
+          <>
+            <Label
+              label="비밀번호"
+              type="password"
+              placeholder="비밀번호를 입력해 주세요."
+              {...register("password")}
+            />
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password.message}</p>
+            )}
+
+            <Label
+              label="비밀번호 확인"
+              type="password"
+              placeholder="비밀번호를 확인"
+              {...register("passwordCheck")}
+            />
+            {errors.passwordCheck && (
+              <p className="text-red-500 text-sm">
+                {errors.passwordCheck.message}
+              </p>
+            )}
+          </>
+        )}
+
+        {step === 3 && (
+          <>
+            <Label
+              label="이름"
+              placeholder="이름을 입력해 주세요."
+              {...register("name")}
+            />
+            {errors.name && (
+              <p className="text-red-500 text-sm">{errors.name.message}</p>
+            )}
+
+            <Label
+              label="이메일"
+              placeholder="name@example.com"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
+
+            <Label
+              label="나이"
+              placeholder="숫자로 입력"
+              {...register("age")}
+            />
+            {errors.age && (
+              <p className="text-red-500 text-sm">{errors.age.message}</p>
+            )}
+          </>
+        )}
+      </AuthForm>
+    </div>
+  );
 };
 
 export default SignUp;
