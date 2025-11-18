9f055c9576b704d8be68634180e8ce7e694c20f7
Sohyunnnn <thgus05061@naver.com>
2025-11-19 02:15:48 +0900

    feat: 로그인 페이지 퍼블리싱

--- Week4/homework/src/pages/sign-in.tsx
+++ Week4/homework/src/pages/sign-in.tsx
@@ -1,5 +1,26 @@
+import AuthForm from "../components/auth-form";
+import Label from "../components/label";
+
 const SignIn = () => {
-  return <div>sign-in</div>;
+  return (
+    <div className="min-h-screen flex items-center w-full">
+      <AuthForm
+        title="로그인"
+        linkTo="/sign-up"
+        linkText="회원가입"
+        buttonText="로그인"
+        buttonType="submit"
+      >
+        <Label label="아이디" name="id" placeholder="아이디를 입력해 주세요." />
+        <Label
+          label="비밀번호"
+          name="password"
+          type="password"
+          placeholder="비밀번호를 입력해 주세요."
+        />
+      </AuthForm>
+    </div>
+  );
 };
 
 export default SignIn;
