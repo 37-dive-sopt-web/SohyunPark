e088e0385d289554dad451af132499b1fd18cb1c
Sohyunnnn <thgus05061@naver.com>
2025-11-19 16:16:16 +0900

    refactor: method 개선

--- Week4/homework/src/apis/common/method.ts
+++ Week4/homework/src/apis/common/method.ts
@@ -1,27 +1,48 @@
 import axiosInstance from "../config/instance";
 
+interface ApiWrapper<T> {
+  success: boolean;
+  code: string;
+  message: string;
+  data: T;
+}
 
-export const apiGet = async <T, P = undefined>(url: string, params?: P): Promise<T> => {
-  const response = await axiosInstance.get<T>(url, { params });
-  return response.data;
+export const apiGet = async <T, P = unknown>(
+  url: string,
+  params?: P
+): Promise<T> => {
+  const response = await axiosInstance.get<ApiWrapper<T>>(url, { params });
+  return response.data.data;
 };
 
-export const apiPost = async <T, B = undefined>(url: string, body?: B): Promise<T> => {
-  const response = await axiosInstance.post<T>(url, body);
-  return response.data;
+export const apiPost = async <T, B = unknown>(
+  url: string,
+  body?: B
+): Promise<T> => {
+  const response = await axiosInstance.post<ApiWrapper<T>>(url, body);
+  return response.data.data;
 };
 
-export const apiPatch = async <T, B = undefined>(url: string, body?: B): Promise<T> => {
-  const response = await axiosInstance.patch<T>(url, body);
-  return response.data;
+export const apiPatch = async <T, B = unknown>(
+  url: string,
+  body?: B
+): Promise<T> => {
+  const response = await axiosInstance.patch<ApiWrapper<T>>(url, body);
+  return response.data.data;
 };
 
-export const apiPut = async <T, B = undefined>(url: string, body?: B): Promise<T> => {
-  const response = await axiosInstance.put<T>(url, body);
-  return response.data;
+export const apiPut = async <T, B = unknown>(
+  url: string,
+  body?: B
+): Promise<T> => {
+  const response = await axiosInstance.put<ApiWrapper<T>>(url, body);
+  return response.data.data;
 };
 
-export const apiDelete = async <T, P = undefined>(url: string, params?: P): Promise<T> => {
-  const response = await axiosInstance.delete<T>(url, { params });
-  return response.data;
+export const apiDelete = async <T, P = unknown>(
+  url: string,
+  params?: P
+): Promise<T> => {
+  const response = await axiosInstance.delete<ApiWrapper<T>>(url, { params });
+  return response.data.data;
 };
